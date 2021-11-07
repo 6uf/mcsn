4bbfb0534e743bfc53a8b2cde66c3ec521750075
Liza-Developer <86650306+Liza-Developer@users.noreply.github.com>
2021-11-07 01:43:24 -0300

    Bug handling and less issues.

--- utils.go
+++ utils.go
@@ -8,31 +8,49 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Liza-Developer/mcapi"
-)
-
-type jsonValues struct {
-	Accounts []string
-	Bearers  []string
-	Config   []string
-}
-
-var (
-	bearers       mcapi.MCbearers
-	config        map[string]interface{}
-	err           error
-	accounts      []string
-	removeBearers []string
-	accountType   []string
+	"github.com/Liza-Developer/mcap"
 )
 
 func init() {
 	q, _ := ioutil.ReadFile("accounts.json")
 
-	config = GetConfig(q)
+	config = mcap.GetConfig(q)
 
-	if len(config[`Bearers`].([]interface{})) == 0 {
-		reauth()
+	if config[`Bearers`] == nil {
+		func() {
+			var configOptions []string
+			for _, accs := range config[`Accounts`].([]interface{}) {
+				accounts = append(accounts, accs.(string))
+			}
+
+			for _, accs := range config[`Config`].([]interface{}) {
+				configOptions = append(configOptions, accs.(string))
+			}
+
+			bearers, err = mcap.Auth(accounts)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			if bearers.Bearers == nil {
+				bearers.Bearers = []string{}
+			}
+
+			var newBearers []string
+
+			for e, bearerz := range bearers.Bearers {
+				newBearers = append(newBearers, bearerz+"`"+time.Now().Add(time.Duration(time.Second*86400)).Format(time.RFC850)+"`"+bearers.AccountType[e])
+			}
+
+			v, _ := json.MarshalIndent(jsonValues{Accounts: accounts, Bearers: newBearers, Config: configOptions}, "", "  ")
+
+			ioutil.WriteFile("accounts.json", v, 0)
+
+			if len(newBearers) == 0 {
+				fmt.Println("No valid accounts.")
+				os.Exit(0)
+			}
+		}()
 	} else {
 		for _, accs := range config[`Bearers`].([]interface{}) {
 
@@ -40,7 +58,7 @@ func init() {
 
 			wdad, _ := time.Parse(time.RFC850, m[1])
 
-			if time.Now().After(wdad) == true {
+			if time.Now().After(wdad) {
 			} else {
 				removeBearers = append(removeBearers, m[0]+"`"+m[1]+"`"+m[2])
 				accounts = append(accounts, m[0])
@@ -63,81 +81,50 @@ func init() {
 			bearers.AccountType = accountType
 		}
 
-		jsonValues := jsonValues{
-			Accounts: m,
-			Bearers:  removeBearers,
-			Config:   configOptions,
-		}
-
-		v, _ := json.MarshalIndent(jsonValues, "", "  ")
+		v, _ := json.MarshalIndent(jsonValues{Accounts: m, Bearers: removeBearers, Config: configOptions}, "", "  ")
 
 		ioutil.WriteFile("accounts.json", v, 0)
 
 		if len(bearers.Bearers) == 0 {
 			fmt.Println("No valid accounts.. Attempting to reauth..")
-			reauth()
+			func() {
+				var configOptions []string
+				for _, accs := range config[`Accounts`].([]interface{}) {
+					accounts = append(accounts, accs.(string))
+				}
+
+				for _, accs := range config[`Config`].([]interface{}) {
+					configOptions = append(configOptions, accs.(string))
+				}
+
+				bearers, err = mcap.Auth(accounts)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				if bearers.Bearers == nil {
+					bearers.Bearers = []string{}
+				}
+
+				var newBearers []string
+
+				for e, bearerz := range bearers.Bearers {
+					newBearers = append(newBearers, bearerz+"`"+time.Now().Add(time.Duration(time.Second*86400)).Format(time.RFC850)+"`"+bearers.AccountType[e])
+				}
+
+				v, _ := json.MarshalIndent(jsonValues{Accounts: accounts, Bearers: newBearers, Config: configOptions}, "", "  ")
+
+				ioutil.WriteFile("accounts.json", v, 0)
+
+				if len(newBearers) == 0 {
+					fmt.Println("No valid accounts.")
+					os.Exit(0)
+				}
+			}()
 		}
 	}
-
 }
-
-func GetConfig(owo []byte) map[string]interface{} {
-	var config map[string]interface{}
-	json.Unmarshal(owo, &config)
-	return config
-}
-
-func remove(l []string, item string) []string {
-	for i, other := range l {
-		if other == item {
-			l = append(l[:i], l[i+1:]...)
-		}
-	}
-	return l
-}
-
 
 func formatTime(t time.Time) string {
 	return t.Format("15:04:05.00000")
-}
-
-func reauth() {
-	var configOptions []string
-	for _, accs := range config[`Accounts`].([]interface{}) {
-		accounts = append(accounts, accs.(string))
-	}
-
-	for _, accs := range config[`Config`].([]interface{}) {
-		configOptions = append(configOptions, accs.(string))
-	}
-
-	bearers, err = mcapi.Auth(accounts)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if bearers.Bearers == nil {
-		bearers.Bearers = []string{}
-	}
-
-	var newBearers []string
-
-	for e, bearerz := range bearers.Bearers {
-		newBearers = append(newBearers, bearerz+"`"+time.Now().Add(time.Duration(time.Second*86400)).Format(time.RFC850)+"`"+bearers.AccountType[e])
-	}
-
-	jsonValues := jsonValues{
-		Accounts: accounts,
-		Bearers:  newBearers,
-		Config:   configOptions,
-	}
-
-	v, _ := json.MarshalIndent(jsonValues, "", "  ")
-
-	ioutil.WriteFile("accounts.json", v, 0)
-
-	if len(newBearers) == 0 {
-		fmt.Println("No valid accounts.")
-		os.Exit(0)
-	}
 }
