0a0b293d25eeb9ce6ac455dee83e52123343d543
Brandon R <brandon.p.r@hotmail.com>
2022-02-15 03:51:10 -0400

    Fix to proxy sniping, added user:pass

--- utils.go
+++ utils.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"image"
@@ -95,7 +96,7 @@ type Details struct {
 }
 
 type Conns struct {
-	Client  *tls.Conn
+	Client  net.Conn
 	Payload string
 }
 
@@ -112,20 +113,30 @@ var (
 	firstRow  [][]int = [][]int{{64, 0, 72, 8}, {56, 0, 64, 8}, {48, 0, 56, 8}, {40, 0, 48, 8}, {32, 0, 40, 8}, {24, 0, 32, 8}, {16, 0, 24, 8}, {8, 0, 16, 8}, {0, 0, 8, 8}}
 )
 
-const rootCert = `-----BEGIN CERTIFICATE-----
-MIIB+TCCAZ+gAwIBAgIJAL05LKXo6PrrMAoGCCqGSM49BAMCMFkxCzAJBgNVBAYT
-AkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRn
-aXRzIFB0eSBMdGQxEjAQBgNVBAMMCWxvY2FsaG9zdDAeFw0xNTEyMDgxNDAxMTNa
-Fw0yNTEyMDUxNDAxMTNaMFkxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0
-YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQxEjAQBgNVBAMM
-CWxvY2FsaG9zdDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHGaaHVod0hLOR4d
-66xIrtS2TmEmjSFjt+DIEcb6sM9RTKS8TZcdBnEqq8YT7m2sKbV+TEq9Nn7d9pHz
-pWG2heWjUDBOMB0GA1UdDgQWBBR0fqrecDJ44D/fiYJiOeBzfoqEijAfBgNVHSME
-GDAWgBR0fqrecDJ44D/fiYJiOeBzfoqEijAMBgNVHRMEBTADAQH/MAoGCCqGSM49
-BAMCA0gAMEUCIEKzVMF3JqjQjuM2rX7Rx8hancI5KJhwfeKu1xbyR7XaAiEA2UT7
-1xOP035EcraRmWPe7tO0LpXgMxlh2VItpc2uc2w=
------END CERTIFICATE-----
-`
+const rootCert = `
+-- GlobalSign Root R2, valid until Dec 15, 2021
+-----BEGIN CERTIFICATE-----
+MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4G
+A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNp
+Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1
+MDgwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEG
+A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI
+hvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8omUVCxKs+IVSbC9N/hHD6ErPL
+v4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7SqbKSaZeqKeMWhG8
+eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQBoZfXklq
+tTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd
+C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pa
+zq+r1feqCapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCB
+mTAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IH
+V2ccHsBqBt5ZtJot39wZhi4wNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5n
+bG9iYWxzaWduLm5ldC9yb290LXIyLmNybDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG
+3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEAmYFThxxol4aR7OBKuEQLq4Gs
+J0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkIk7mpM0sYmsL4h4hO
+291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRDLenVOavS
+ot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd
+AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7
+TBj0/VLZjmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg==
+-----END CERTIFICATE-----`
 
 func init() {
 	if runtime.GOOS == "windows" {
@@ -603,7 +614,7 @@ func snipe(name string, delay float64, option string, charType string) {
 							SentAt:     sendTime,
 							RecvAt:     recvTime,
 							StatusCode: string(ea[9:12]),
-							Success:    strings.Contains(string(ea[9:12]), "200"),
+							Success:    string(ea[9:12]) == "200",
 							UnixRecv:   recvTime.Unix(),
 							Email:      account.Email,
 							Type:       account.AccountType,
@@ -873,9 +884,10 @@ func checkVer(name string, delay float64, dropTime int64) {
 					SentAt:     sendTime,
 					RecvAt:     recvTime,
 					StatusCode: string(ea[9:12]),
-					Success:    strings.Contains(string(ea[9:12]), "200"),
+					Success:    string(ea[9:12]) == "200",
 					UnixRecv:   recvTime.Unix(),
 					Email:      account.Email,
+					Type:       account.AccountType,
 				})
 
 				wg.Done()
@@ -1074,9 +1086,11 @@ func proxy(name string, delay float64, dropTime int64) {
 		time.Sleep(time.Second * 1)
 	}
 
+	fmt.Println()
+
 	clients := genSockets(proxys, name)
 
-	fmt.Print("\n\n")
+	fmt.Println()
 
 	apiGO.Sleep(dropTime, delay)
 
@@ -1193,6 +1207,7 @@ func setup(proxy []string) {
 
 func genSockets(proxy []string, name string) []Conns {
 	var DataType []Conns
+	var payload string
 
 	roots := x509.NewCertPool()
 	ok := roots.AppendCertsFromPEM([]byte(rootCert))
@@ -1205,20 +1220,25 @@ func genSockets(proxy []string, name string) []Conns {
 			break
 		} else {
 			proxy := randomInt(proxy)
-			conn, err := net.Dial("tcp", proxy)
+
+			conn, err := net.Dial("tcp", strings.Split(proxy, ":")[0]+":"+strings.Split(proxy, ":")[1])
 			if err != nil {
 				sendE(err.Error())
 			} else {
+				if len(strings.Split(proxy, ":")) > 2 {
+					payload = fmt.Sprintf("Proxy-Authorization: Basic %v\r\n", base64.StdEncoding.EncodeToString([]byte(strings.Split(proxy, ":")[2]+":"+strings.Split(proxy, ":")[3])))
+				}
 
-				conn.Write([]byte("CONNECT api.minecraftservices.com:443 HTTP/1.1\r\nHost: api.minecraftservices.com:443\r\nProxy-Connection: keep-alive\r\nUser-Agent: MCSN/1.1\r\n\r\n"))
+				conn.Write([]byte(fmt.Sprintf("CONNECT api.minecraftservices.com:443 HTTP/1.1\r\nHost: api.minecraftservices.com\r\nUser-Agent: MCSN/1.1\r\nProxy-Connection: keep-alive\r\n%v\r\n", payload)))
 
 				var junk = make([]byte, 4096)
-
 				conn.Read(junk)
 
-				config := &tls.Config{RootCAs: roots, InsecureSkipVerify: true, ServerName: strings.Split(proxy, ":")[0]}
+				if string(junk[9:12]) == "200" {
+					sendS(fmt.Sprintf("Succesfully logged into %v", strings.Split(proxy, ":")[0]))
+				}
 
-				tls := tls.Client(conn, config)
+				tls := tls.Client(conn, &tls.Config{RootCAs: roots, InsecureSkipVerify: true, ServerName: "api.minecraftservices.com"})
 
 				if bearer.AccountType == "Giftcard" {
 					DataType = append(DataType, Conns{
@@ -1228,7 +1248,7 @@ func genSockets(proxy []string, name string) []Conns {
 				} else {
 					DataType = append(DataType, Conns{
 						Client:  tls,
-						Payload: "PUT /minecraft/profile/name/" + name + " HTTP/1.1\r\nHost: api.minecraftservices.com:443\r\nUser-Agent: MCSN/1.0\r\nAuthorization: bearer " + bearer.Bearer + "\r\n\r\n",
+						Payload: "PUT /minecraft/profile/name/" + name + " HTTP/1.1\r\nHost: api.minecraftservices.com\r\nUser-Agent: MCSN/1.0\r\nAuthorization: bearer " + bearer.Bearer + "\r\n\r\n",
 					})
 				}
 			}
@@ -1237,5 +1257,3 @@ func genSockets(proxy []string, name string) []Conns {
 
 	return DataType
 }
-
-//
