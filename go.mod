8baf7f588a67795ea1bbeeff4fec6d709455c36c
Brandon R <brandon.p.r@hotmail.com>
2022-03-28 17:27:49 -0200

    greatly reduced libs (most unneeded) cleaned up config vars etc.

--- go.mod
+++ go.mod
@@ -8,7 +8,12 @@ require (
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/logrusorgru/aurora/v3 v3.0.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/urfave/cli/v2 v2.4.0
 	golang.org/x/net v0.0.0-20220325170049-de3da57026de
 )
 
-require golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,311 @@
+package main
+
+import (
+	"fmt"
+	"mcsn/src"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/logrusorgru/aurora/v3"
+	"github.com/urfave/cli/v2"
+)
+
+func init() {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+
+	src.CheckFiles()
+	src.Acc.LoadState()
+
+	src.Pro = src.GenProxys()
+	src.Setup(src.Pro)
+
+	header := `
+ •   ▄ ·   ▄▄·  ▄▄ ·    ▄ 
+·██ ▐███▪▐█ ▌▪▐█ ▀  •█▌▐█
+▐█ ▌▐▌▐█·██ ▄▄▄▀▀▀█▄▐█▐▐▌
+██ ██▌▐█▌▐███▌▐█▄▪▐███▐█▌
+▀▀  █▪▀▀▀·▀▀▀  ▀▀▀▀ ▀▀ █▪
+`
+
+	for _, char := range []string{"•", "·", "▪"} {
+		header = strings.ReplaceAll(header, char, aurora.Sprintf(aurora.Faint(aurora.Red("%v")), char))
+	}
+	for _, char := range []string{"█", "▄", "▌", "▀", "▌", "▀"} {
+		header = strings.ReplaceAll(header, char, aurora.Sprintf(aurora.Faint(aurora.White("%v")), char))
+	}
+	for _, char := range []string{"▐"} {
+		header = strings.ReplaceAll(header, char, aurora.Sprintf(aurora.Faint(aurora.BrightBlack("%v")), char))
+	}
+
+	fmt.Println(header)
+}
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "snipe",
+				Usage: `Snipes names onto an src.Account.`,
+				Action: func(c *cli.Context) error {
+					src.AuthAccs()
+					fmt.Println()
+					go src.CheckAccs()
+					src.Snipe(c.String("u"), c.Float64("d"), "single", "")
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "u",
+						Usage: "username to snipe",
+					},
+					&cli.Float64Flag{
+						Name:  "d",
+						Usage: "Snipes a few ms earlier so you can counter ping lag.",
+					},
+				},
+			},
+
+			{
+				Name:    "auto",
+				Aliases: []string{"as", "a"},
+				Usage:   "Auto sniper attempts to snipe upcoming 3 character usernames.",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "3c",
+						Usage: "Snipe names are are a combination of Numeric and Alphabetic.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe("", c.Float64("d"), "auto", "3c")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+					{
+						Name:  "3l",
+						Usage: "Snipe only Alphabetic names.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe("", c.Float64("d"), "auto", "3l")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+					{
+						Name:  "3n",
+						Usage: "Snipe only Numeric names.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe("", c.Float64("d"), "auto", "3n")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+
+					{
+						Name:  "list",
+						Usage: "Snipe names are are a combination of Numeric and Alphabetic.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe("", c.Float64("d"), "auto", "list")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+				},
+			},
+
+			{
+				Name:    "ping",
+				Aliases: []string{"p"},
+				Usage:   "ping helps give you a rough estimate of your connection to the minecraft API.",
+				Action: func(c *cli.Context) error {
+					delay, time := src.MeanPing()
+					fmt.Printf("Estimated (Mean) Delay: %v ~ Took: %v\n", delay, time)
+					return nil
+				},
+			},
+
+			{
+				Name:    "proxy",
+				Aliases: []string{"p"},
+				Usage:   "Proxy snipes names for you",
+				Action: func(c *cli.Context) error {
+					src.AuthAccs()
+					fmt.Println()
+					go src.CheckAccs()
+					src.Snipe(c.String("u"), c.Float64("d"), "proxy", "")
+					return nil
+				},
+
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "u",
+						Usage: "username to snipe",
+					},
+					&cli.Float64Flag{
+						Name:  "d",
+						Usage: "Snipes a few ms earlier so you can counter ping lag.",
+					},
+				},
+
+				Subcommands: []*cli.Command{
+					{
+						Name:  "3c",
+						Usage: "Snipe names are are a combination of Numeric and Alphabetic.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe(c.String("u"), c.Float64("d"), "proxy", "3c")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+					{
+						Name:  "3l",
+						Usage: "Snipe only Alphabetic names.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe(c.String("u"), c.Float64("d"), "proxy", "3l")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+					{
+						Name:  "3n",
+						Usage: "Snipe only Numeric names.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe(c.String("u"), c.Float64("d"), "proxy", "3n")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+					{
+						Name:  "list",
+						Usage: "Snipe only Numeric names.",
+						Action: func(c *cli.Context) error {
+							src.AuthAccs()
+							fmt.Println()
+							go src.CheckAccs()
+							src.Snipe(c.String("u"), c.Float64("d"), "proxy", "list")
+							return nil
+						},
+						Flags: []cli.Flag{
+							&cli.Float64Flag{
+								Name:  "d",
+								Usage: "Snipes a few ms earlier so you can counter ping lag.",
+							},
+						},
+					},
+				},
+			},
+
+			{
+				Name:    "turbo",
+				Aliases: []string{"t"},
+				Usage:   "Turbo a name just in case it drops!",
+				Action: func(c *cli.Context) error {
+					src.AuthAccs()
+					fmt.Println()
+					go src.CheckAccs()
+					src.Snipe(c.String("u"), float64(c.Int64("t")), "turbo", "")
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "u",
+						Usage: "username to snipe",
+					},
+					&cli.Int64Flag{
+						Name:  "t",
+						Usage: "the seconds between each request",
+						Value: 60,
+					},
+				},
+			},
+
+			{
+				Name:    "namemc",
+				Aliases: []string{"n", "nmc", "skinart"},
+				Usage:   `NameMC Skin Art`,
+				Action: func(c *cli.Context) error {
+					src.Skinart(c.String("n"), c.String("i"))
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "n",
+						Usage: "Name of your Art (this will be used for the folder name)",
+					},
+					&cli.StringFlag{
+						Name:  "i",
+						Usage: "Name of your image.",
+					},
+				},
+			},
+		},
+		Name:    "MCSN",
+		Usage:   "A name sniper dedicated to premium free services",
+		Version: "5.15",
+	}
+
+	app.Run(os.Args)
+}
